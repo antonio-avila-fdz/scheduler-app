19316056650cb1f0738c6c43d0514012fd734294
Antonio Avila <antonio.avila.dev@gmail.com>
2025-03-03 22:49:09 -0600

    added database, updated repository

---
+++ app/Contracts/UserRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Contracts;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+
+interface UserRepositoryInterface
+{
+    public function getAll(): Collection;
+    public function getByTimezones(array $timezones): Collection;
+    public function create(Request $request): User;
+}
